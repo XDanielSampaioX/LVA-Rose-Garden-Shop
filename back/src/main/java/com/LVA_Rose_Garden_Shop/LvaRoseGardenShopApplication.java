00a8bc28aa5f87cc9de4e3f33dbeaee016aae16f
Daniel Sampaio <danielsampaio127@gmail.com>
2025-03-14 12:37:11 -0300

    feat: criando projeto

---
+++ back/src/main/java/com/LVA_Rose_Garden_Shop/LvaRoseGardenShopApplication.java
@@ -0,0 +1,13 @@
+package com.LVA_Rose_Garden_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvaRoseGardenShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvaRoseGardenShopApplication.class, args);
+	}
+
+}
