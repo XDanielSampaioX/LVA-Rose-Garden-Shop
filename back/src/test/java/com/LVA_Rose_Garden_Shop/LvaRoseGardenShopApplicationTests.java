d86a273f2bf59e8a07b472a4c4d3881a63b4ad75
Daniel Sampaio <danielsampaio127@gmail.com>
2025-03-14 12:37:11 -0300

    feat: criando projeto

---
+++ back/src/test/java/com/LVA_Rose_Garden_Shop/LvaRoseGardenShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LVA_Rose_Garden_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvaRoseGardenShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
