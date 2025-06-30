15cae7f42cd9fa7559411e3d15ab48905b696d1d
Daniel Sampaio <danielsampaio127@gmail.com>
2025-06-30 15:43:40 -0300

    feat: crindo paginacao com filtros usando specification

--- back/src/main/java/com/LVA_Rose_Garden_Shop/repository/ProdutoRepository.java
+++ back/src/main/java/com/LVA_Rose_Garden_Shop/repository/ProdutoRepository.java
@@ -2,8 +2,9 @@ package com.LVA_Rose_Garden_Shop.repository;
 
 import com.LVA_Rose_Garden_Shop.domain.produto.ProdutoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long> {
+public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long>, JpaSpecificationExecutor<ProdutoEntity> {
 }
--- back/src/main/java/com/LVA_Rose_Garden_Shop/specification/ProdutoSpecification.java
+++ back/src/main/java/com/LVA_Rose_Garden_Shop/specification/ProdutoSpecification.java
@@ -1,4 +1,41 @@
 package com.LVA_Rose_Garden_Shop.specification;
 
+import com.LVA_Rose_Garden_Shop.domain.produto.ProdutoEntity;
+import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class ProdutoSpecification {
+    public static Specification<ProdutoEntity> comParametros(String nome,
+                                                             String descricao,
+                                                             BigDecimal preco,
+                                                             String categoria,
+                                                             Long estoque) {
+        return (Root<ProdutoEntity> root, CriteriaQuery<?> query, CriteriaBuilder builder) -> {
+            Predicate predicate = builder.conjunction();
+
+            if (!StringUtils.isEmpty(nome)) {
+                predicate = builder.and(predicate, builder.like(root.get("nome"), "%" + nome + "%"));
+            }
+            if (!StringUtils.isEmpty(descricao)) {
+                predicate = builder.and(predicate, builder.like(root.get("descricao"), "%" + descricao + "%"));
+            }
+            if (Objects.nonNull(preco)) {
+                predicate = builder.and(predicate, builder.equal(root.get("preco"), preco));
+            }
+            if (!StringUtils.isEmpty(categoria)) {
+                predicate = builder.and(predicate, builder.like(root.get("categoria"), "%" + categoria + "%"));
+            }
+            if (Objects.nonNull(estoque)) {
+                predicate = builder.and(predicate, builder.equal(root.get("estoque"), estoque));
+            }
+            return predicate;
+        };
+    }
 }
